6199fa138b398062b7df7b8b9f5056c971c658bc
Nathanael Rensen <nathanael@polymorpheus.com>
2015-08-01 22:55:14 +0800

    Fix replying to comments on articles

--- core/modules/main/templates/_comment.inc.php
+++ core/modules/main/templates/_comment.inc.php
@@ -79,7 +79,19 @@
                 <option value="0"<?php if (!$comment->isPublic()): ?> selected="selected" <?php endif; ?>><?php echo __('Visible for me, developers and administrators only'); ?></option>
             </select>
             <br />
-            <?php include_component('main/textarea', array('area_name' => 'comment_body', 'target_type' => $comment->getTargetType(), 'target_id' => $comment->getTargetId(), 'area_id' => 'comment_reply_'.$comment->getID().'_bodybox', 'height' => '200px', 'width' => '100%', 'syntax' => \thebuggenie\core\framework\Settings::getSyntaxClass($comment->getSyntax()), 'value' => tbg_decodeUTF8("\n\n\n'''".__('%user wrote:', array('%user' => ($comment->getPostedBy() instanceof \thebuggenie\core\entities\common\Identifiable) ? $comment->getPostedBy()->getName() : __('Unknown user')))."'''\n>".str_replace("\n", "\n> ", wordwrap(html_entity_decode(strip_tags(tbg_decodeUTF8($comment->getContent(), true)), ENT_COMPAT, \thebuggenie\core\framework\Context::getI18n()->getCharset()), 75, "\n"))."\n", true))); ?>
+            <?php
+                $bold = '';
+                switch($comment->getSyntax())
+                {
+                case \thebuggenie\core\framework\Settings::SYNTAX_MD:
+                    $bold = "**";
+                    break;
+                case \thebuggenie\core\framework\Settings::SYNTAX_MW:
+                    $bold = "'''";
+                    break;
+                }
+            ?>
+            <?php include_component('main/textarea', array('area_name' => 'comment_body', 'target_type' => $comment->getTargetType(), 'target_id' => $comment->getTargetId(), 'area_id' => 'comment_reply_'.$comment->getID().'_bodybox', 'height' => '200px', 'width' => '100%', 'syntax' => \thebuggenie\core\framework\Settings::getSyntaxClass($comment->getSyntax()), 'value' => tbg_decodeUTF8("\n\n\n{$bold}".__('%user wrote:', array('%user' => ($comment->getPostedBy() instanceof \thebuggenie\core\entities\common\Identifiable) ? $comment->getPostedBy()->getName() : __('Unknown user')))."{$bold}\n>".str_replace("\n", "\n> ", wordwrap(html_entity_decode(strip_tags(tbg_decodeUTF8($comment->getContent(), true)), ENT_COMPAT, \thebuggenie\core\framework\Context::getI18n()->getCharset()), 75, "\n"))."\n", true))); ?>
             <div id="comment_reply_indicator_<?php echo $comment->getID(); ?>" style="display: none;">
                 <?php echo image_tag('spinning_16.gif', array('class' => 'spinning')); ?>
             </div>
